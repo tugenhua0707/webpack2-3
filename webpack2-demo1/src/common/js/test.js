6fbf1ddb26b21c19f84b2011b7c7cbee27adc338
tugenhua <tugenhua@tugenhuadeMacBook-Pro.local>
2017-07-01 18:00:02 +0800

    webpack2项目构建

---
+++ webpack2-demo1/src/common/js/test.js
@@ -0,0 +1,9 @@
+function foo() {
+	let name = 2555;
+	console.debug(name);
+}
+
+function foo2() {
+	console.debug('33555');
+}
+export {foo};
---
+++ webpack2-demo1/src/main.js
@@ -0,0 +1,7 @@
+require('./common/css/style.css');
+import './common/css/stylus.styl';
+
+import {foo} from './common/js/test.js'
+
+console.info('2111');
+foo();
